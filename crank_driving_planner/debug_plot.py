bug fix: calculation eular from quaternion

crank_driving_planner/debug_plot.py
@@ -5,10 +5,18 @@ from matplotlib.patches import Rectangle
 
 class PlotMarker():
     def __init__(self):
-        self.plot_width = 20
-        self.plot_forward = 40
-        self.plot_backward = 5
+        self.plot_width = 50
+        self.plot_forward = 20
+        self.plot_backward = 40
+        self.map_yaw = math.pi * 43/32
 
+        self.map_rot = np.array([[math.cos(self.map_yaw), math.sin(self.map_yaw)],
+                        [-math.sin(self.map_yaw), math.cos(self.map_yaw)]])     
+        self.map_rot_wh = np.array([[math.cos(self.map_yaw), 0],
+                                    [0, math.sin(self.map_yaw)]])
+        
+        self.robot_length = 3.5
+        self.robot_width = 1.5
     def plot_status(self,
                     ego_pose = None,
                     object_pose = None,
@@ -16,41 +24,77 @@ class PlotMarker():
                     right_bound = None,
                     index_min= None,
                     index_max=None,
-                    rotation = True,
+                    rotation = False,
                     ):
-        yaw = math.pi * 43/32
+        
         if ego_pose is not None:
             plt.cla()
 
+            ego_pose_origin = ego_pose
             if rotation:
-                rot = np.array([[math.cos(yaw), math.sin(yaw)],
-                                [-math.sin(yaw), math.cos(yaw)]])
-                ego_pose_origin = ego_pose[0:2] 
                 ego_pose = np.array([0, 0])
             
-
+            ## Plot ego pose
             plot_xmin = ego_pose[0] - self.plot_width / 2.0
             plot_xmax = ego_pose[0] + self.plot_width / 2.0
             plot_ymin = ego_pose[1] - self.plot_backward/ 2.0
             plot_ymax = ego_pose[1] + self.plot_forward / 2.0
             plt.plot(ego_pose[0], ego_pose[1], "xr")
 
-            if object_pose is not None:
-                if rotation:
-                    object_pose = object_pose[:,0:2] - ego_pose_origin
-                    object_pose = object_pose[:,0:2] @ rot
-                plt.plot(object_pose[:,0], object_pose[:,1], "ob")
+            yaw = ego_pose_origin[2]
+            print(math.sin(yaw), math.cos(yaw))
+            ego_rot = np.array([[math.cos(yaw), math.sin(yaw)],
+                            [-math.sin(yaw), math.cos(yaw)]])
 
+            outline = np.array([
+                               [-self.robot_length / 2, self.robot_length / 2,
+                                (self.robot_length / 2), -self.robot_length / 2,
+                                -self.robot_length / 2],
+                                [self.robot_width / 2, self.robot_width / 2,
+                                - self.robot_width / 2, -self.robot_width / 2,
+                                self.robot_width / 2]
+                                ])
+            outline = (outline.T.dot(ego_rot)).T
+            
+            outline[0, :] += ego_pose[0]
+            outline[1, :] += ego_pose[1]
+            plt.plot(np.array(outline[0, :]).flatten(),
+                    np.array(outline[1, :]).flatten(), "-k")
+            
+
+            ego_vec = np.array([np.cos(yaw), np.sin(yaw)]) *3
+            ego_vec = ego_pose[0:2] + ego_vec
+            trajec = np.array([[ego_pose[0], ego_pose[1]],
+                              [ego_vec[0], ego_vec[1]]])
+            plt.plot(trajec[:,0], trajec[:,1], "red")            
+
+            ## Plot object pose
+            if object_pose is not None:
+                object_xy = object_pose[:,0:2] 
+                object_wh = object_pose[:,2:4]
+                if rotation:
+                    object_xy = object_xy[:,0:2] - ego_pose_origin[0:2] 
+                    object_xy = object_xy[:,0:2] @ self.map_rot
+                    object_wh = object_wh[:,0:2] @ self.map_rot_wh
+                else:
+                    object_xy_min = object_xy - object_wh / 2.0
+                    object_xy_max = object_xy + object_wh / 2.0     
+                object_xy_min = object_xy - object_wh / 2.0
+                object_xy_max = object_xy + object_wh / 2.0
+                plt.plot(object_xy_min[:,0], object_xy_min[:,1], "ob")
+                plt.plot(object_xy_max[:,0], object_xy_max[:,1], "ob")
+
+            ## Plot path pose
             if (left_bound is not None) and (right_bound is not None):
                 if rotation:
-                    left_bound = left_bound[:,0:2] - ego_pose_origin
-                    left_bound = left_bound[:,0:2] @ rot
+                    left_bound = left_bound[:,0:2] - ego_pose_origin[0:2] 
+                    left_bound = left_bound[:,0:2] @ self.map_rot
 
-                    right_bound = right_bound[:,0:2] - ego_pose_origin
-                    right_bound = right_bound[:,0:2] @ rot
-
-                plt.plot(left_bound[:, 0], left_bound[:, 1], color="black")
-                plt.plot(right_bound[:,0], right_bound[:,1], color="black")
+                    right_bound = right_bound[:,0:2] - ego_pose_origin[0:2] 
+                    right_bound = right_bound[:,0:2] @ self.map_rot
+                print(left_bound)
+                plt.plot(left_bound[0:2, 0], left_bound[0:2, 1], color="black")
+                plt.plot(right_bound[0:2,0], right_bound[0:2,1], color="black")
 
                 if (index_min is not None) and (index_max is not None):
                     plt.plot(left_bound[index_min:index_max + 1 , 0], left_bound[index_min:index_max + 1, 1], color="green")
@@ -58,7 +102,7 @@ class PlotMarker():
                     
             plt.xlim(plot_xmin, plot_xmax)
             plt.ylim(plot_ymin, plot_ymax)
-            #plt.grid(True)
+            plt.grid(True)
             plt.pause(0.01)
             #plt.show()
 

crank_driving_planner/predicted_objects_info.py
@@ -12,6 +12,8 @@ class PredictedObjectsInfo:
         self.show_objects_plot = show_objects_plot
         self.objects_rectangle = np.empty((0,4))
 
+        self.objects_vis_box =  []
+
         self.objects_labels = []
         for idx in range(len(predicted_obj)):
             ob = predicted_obj[idx]
@@ -23,7 +25,7 @@ class PredictedObjectsInfo:
             shape_array = self._getShapeArrayfromPredictedObjectKinematics(ob.shape)
             
             if len(shape_array) == 0:
-                continue
+                shape_array = [[2.5, 2.5],[-2.5, -2.5]]
 
             object_points = shape_array  + pose
 
@@ -36,12 +38,18 @@ class PredictedObjectsInfo:
             length_x = abs(xmax - xmin)
             length_y = abs(ymax - ymin)
             rect = np.array([cx, cy ,length_x ,length_y])
+            vis_box = [[xmin, ymin], [xmin, ymax], [xmax, ymax], [xmax, ymin], [xmin, ymin]]
+
+            self.objects_vis_box.append(vis_box)
             self.objects_rectangle = np.vstack([self.objects_rectangle, rect])
 
             if self.show_objects_plot:
                 object_points = np.vstack([object_points, object_points[0]])
                 self.show_objects(self.objects_rectangle)
-            
+
+        if self.objects_vis_box is not None:
+            self.objects_vis_box = np.array(self.objects_vis_box)
+
     def _getInitialPosesfromPredictedObjectKinematics(self, kinematics: PredictedObjectKinematics):
         x = kinematics.initial_pose_with_covariance.pose.position.x
         y = kinematics.initial_pose_with_covariance.pose.position.y

crank_driving_planner/trajectory_uitl.py
@@ -1,6 +1,7 @@
 
 from autoware_auto_planning_msgs.msg import Trajectory
 from geometry_msgs.msg import Point
+import math
 import numpy as np
 
 
@@ -35,9 +36,9 @@ def getNearestPointIndex(point, points) -> int:
     return idx 
 
 def ConvertPoint2List(p) -> np.array:
-    div = p.orientation.x ** 2 + p.orientation.y **2 - p.orientation.z **2 - p.orientation.w **2
-    yaw = np.arctan(2 *(p.orientation.x * p.orientation.w +  p.orientation.y * p.orientation.z)
-                        / div)
+    siny_cosp = 2 * (p.orientation.w * p.orientation.z + p.orientation.x * p.orientation.y)
+    cosy_cosp = 1 - 2 * (p.orientation.y * p.orientation.y + p.orientation.z * p.orientation.z)
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
     return np.array([p.position.x, p.position.y, yaw])
 
 