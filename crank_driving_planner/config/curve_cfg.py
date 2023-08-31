be0905087be0ce369a3cf6783bf58f3fc4ab2825
bushio <kuromady@gmail.com>
2023-08-31 22:33:34 +0900

    update curve_parameter

--- crank_driving_planner/config/curve_cfg.py
+++ crank_driving_planner/config/curve_cfg.py
@@ -2,15 +2,15 @@
 
 class CurveConfig:
     def __init__(self):
-        self.circle_radius = [4.0, 4.5, 3.0, 7.5] #[m]
+        self.circle_radius = [4.0, 4.5, 3.0, 4.5] #[m]
         self.circle_angle_rate = [0.7, 0.6, 0.4, 0.3] 
 
-        self.inner_start_mergin = [2.0, 2.0, 1.5, 3.5] #[m]
+        self.inner_start_mergin = [2.0, 2.0, 1.5, 0.1] #[m]
         self.inner_finish_mergin = [5.0, 5.0, 1.0, 1.0] #[m]
 
         self.exec_predict = [False, False, False, True]
 
-        self.sharp_dist_threshold = 15 #[m]
+        self.sharp_dist_threshold = 17 #[m]
         self.animation_flag = False
 
         self.arrival_threthold = 1.5 #[m]
